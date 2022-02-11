7e158a5fa05e6c7f982d49ec2e8314c599adfae4
Jaylon Carter <carterjay2001@outlook.com>
2022-02-11 11:06:50 -0600

    exercises done

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ console.log(
 );
 
 // Exercise 1
+const firstName = 'Jaylon';
+let lastName = 'Carter';
+var age = 20;
+let city = 'Hoover';
+let passtime = 'code';
 
 // Exercise 2
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(`${firstName} ${lastName}`);
 
 // Exercise 3
+let myStory = `Hi my name is ${fullName}. I am ${age} years old and I love to ${passtime} in my hometown of ${city}.`;
+console.log(myStory);
